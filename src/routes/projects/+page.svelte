b0e238abbe036f4946096fd7d325b224719e247e
wamlat <ejdhdhhdhdme2@gmail.com>
2025-04-23 06:48:33 -0400

    test 12

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -72,10 +72,7 @@
     tools that improve people's lives.
   </p>
 
-  <p class="text-lg mb-4">
-    You'll see that I particularly like programming languages, distributed
-    systems, machine learning, computer graphics, music, and art.
-  </p>
+  <p class="text-lg mb-4">You'll see that I particularly like learning.</p>
 
   <p class="text-lg">
     If you find something interesting,
